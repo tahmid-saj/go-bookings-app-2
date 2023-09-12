8dddad7349310d4705dee842c297746025c1a0c5
tahmid-saj <tahmid.saj@gmail.com>
2023-09-11 23:22:16 -0400

    displaying response from post

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -163,8 +163,13 @@ func (m *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request) 
 
 	if !ok {
 		log.Println("cannot get item from session")
+		m.App.Session.Put(r.Context(), "error", "Can't get reservation from session")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+
 		return
 	}
+
+	m.App.Session.Remove(r.Context(), "reservation")
 
 	data := make(map[string]interface{})
 	data["reservation"] = reservation
